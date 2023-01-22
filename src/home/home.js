b30dfe7fb5bde74dc3f58249b83010dde2a68bf0
Yu Ruojie <yuruojie777@163.com>
2023-01-23 00:02:00 +1100

    temp

---
+++ src/home/home.js
@@ -0,0 +1,39 @@
+import './css/home.css';
+
+const Home = ()=>{
+
+
+    return (
+        <div>
+            <div className="header">
+                {/* <h1>header</h1> */}
+                <div className='logo'>
+                    <img src='https://is3-ssl.mzstatic.com/image/thumb/Purple116/v4/45/4e/75/454e75d4-a01b-fd26-c36f-62b6b15d5343/AppIcon-0-0-1x_U007emarketing-0-10-0-sRGB-85-220.png/460x0w.webp' />
+                </div>
+                <h2>Habitly</h2>
+            </div>
+            <div className="product">
+                <div className='productList'>
+                    <img src='https://is1-ssl.mzstatic.com/image/thumb/PurpleSource116/v4/b5/5c/4b/b55c4b51-5701-e4c8-9bca-9e8ceefc1dff/2d4b44d8-3d73-4457-9f55-651dbad4613d_8888.png/460x0w.webp'/>
+                    <img src='https://is1-ssl.mzstatic.com/image/thumb/PurpleSource126/v4/e8/8a/01/e88a0187-56a2-f6af-a495-3cb48c529a91/a57bf9fe-dd2b-4c72-8da0-5de4275e0441_7777.png/460x0w.webp'/>
+                    <img src='https://is5-ssl.mzstatic.com/image/thumb/Purple126/v4/ab/f7/76/abf77679-9e11-8e8b-f86e-dd6872de5344/fc38640d-3029-454b-a69e-144ff5bcbe92_1.png/460x0w.png'/>
+                    <img src='https://is3-ssl.mzstatic.com/image/thumb/Purple116/v4/e0/76/83/e0768323-9bab-e32a-ecc7-05f56d56a7b5/51af1719-4812-4ed4-991f-39ebe07f2914_5.png/460x0w.webp'/>
+                    <img src='https://is2-ssl.mzstatic.com/image/thumb/Purple126/v4/f9/d4/30/f9d430e3-1821-1e49-2937-c3bc53a11a31/3a8b50cf-f23a-4150-8ed7-c3182046ecdc_6.png/460x0w.webp'/>
+                    <img src='https://is1-ssl.mzstatic.com/image/thumb/Purple126/v4/3f/56/46/3f5646af-095e-45a7-e347-bfd4b45768fd/73f1241d-bc28-4150-8563-ea4ca1200f23_3.png/460x0w.webp'/>
+                </div>
+            </div>
+            <div className="footer">
+                {/* <h1>footer</h1> */}
+                <div><a href='https://beian.miit.gov.cn' >京ICP备19058989号-2</a></div>
+                <div>Icons made by < a href=" " title="Vectors Market">Vectors Market</ a> from < a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</ a></div>
+                <div>Icons made by < a href="https://www.freepik.com" title="Freepik">Freepik</ a> from < a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</ a></div>
+                <div>Icons made by < a href="https://icon54.com/" title="Pixel perfect">Pixel perfect</ a> from < a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</ a></div>
+                <div>Photo by <a href="https://www.pexels.com/@tomas-anunziata-129267?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels">Tomas Anunziata</a>from <a href="https://www.pexels.com/photo/man-standing-on-top-of-hill-under-starry-night-3876412/?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels">Pexels</a></div>
+                <div>Photo by <a href="https://www.pexels.com/@sam-willis-457311?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels">Sam Willis</a> from <a href="https://www.pexels.com/photo/man-standing-on-top-of-hill-under-starry-night-3876412/?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels">Pexels</a></div>
+                <div>Photo by <a href="https://www.pexels.com/@h3mins?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels">Hemin Suthar</a> from <a href="https://www.pexels.com/photo/man-standing-on-top-of-hill-under-starry-night-3876412/?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels">Pexels</a></div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/homepage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const HomePage = () => {
+  return (
+    <div>
+        <h1>Habitly</h1>
+        <img src='1024.png'></img>
+    </div>
+  )
+}
